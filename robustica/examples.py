f19cf14f2af0707cf8f6ea403cb21520c00a273e
MiqG <miquelangladagirotto@gmail.com>
2021-03-14 21:23:04 +0100

    improving examples

--- robustica/examples.py
+++ robustica/examples.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # sampledata
-nrow, ncol = 100, 20
-sampledata = pd.DataFrame(np.random.rand(100, 20), 
+nrow, ncol = 20000, 400
+sampledata = pd.DataFrame(np.random.rand(nrow, ncol), 
                           index=['row%s'%r for r in range(nrow)], 
                           columns=['col%s'%c for c in range(ncol)])
