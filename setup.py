78ae70a5c0f6d389ffd4065b217f131659f53b89
MiqG <miquelangladagirotto@gmail.com>
2021-07-06 13:27:42 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name='robustica',
-    packages=setuptools.find_packages()
+setup(
+    name="robustica",
+    version="0.1.0",
+    packages=["robustica"],
+    python_requires=">=3.8",
+    package_data={"": ["LICENSE", "*.md","*.ipynb","*.yml"]},
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "scikit-learn",
+        "scikit-learn-extra",
+        "joblib",
+        "tqdm",
+    ],
+    author="Miquel Anglada Girotto",
+    author_email="miquelangladagirotto@gmail.com",
+    description="Fully cumstomizable robust Independent Components Analysis (ICA)",
+    url="https://github.com/MiqG/robustica",
+    project_urls={"Issues": "https://github.com/MiqG/robustica/issues"},
 )
