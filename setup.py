c45b6f272b4bf67fe2d40151c40d943ecaf6f85a
Miquel Anglada Girotto <45081549+MiqG@users.noreply.github.com>
2021-07-24 15:08:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="robustica",
-    version="0.1.0",
+    version="0.1.1",
     packages=["robustica"],
     python_requires=">=3.8",
     package_data={"": ["LICENSE", "*.md","*.ipynb","*.yml"]},
