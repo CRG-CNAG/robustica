f323e2609d250b3f7945131582c739a7c2273cdb
MiqG <miquelangladagirotto@gmail.com>
2022-07-15 17:14:15 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="robustica",
@@ -18,6 +22,11 @@ setup(
     author="Miquel Anglada Girotto",
     author_email="miquel.anglada@crg.eu",
     description="Fully cumstomizable robust Independent Components Analysis (ICA)",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/CRG-CNAG/robustica",
-    project_urls={"Issues": "https://github.com/CRG-CNAG/robustica/issues"},
+    project_urls={
+        "Issues": "https://github.com/CRG-CNAG/robustica/issues",
+        "Documentation": "https://crg-cnag.github.io/robustica/"
+    },
 )
